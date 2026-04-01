893ece348e652bdab27fcf4a9fcbbf5474116d7e
Ken <kenspicer@comcast.net>
2026-04-01 16:14:36 -0400

    finished chapter1 lesson4(metadata)

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    )
+
+if not response.usage_metadata:
+    raise RuntimeError("Gemini API response appears to be malformed")
+
+    
+print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+#Prompt tokens: X
+#Response tokens: Y
 print(response.text)
